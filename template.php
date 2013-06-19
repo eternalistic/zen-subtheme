3eec4a60a44a06b305fd1044f89d773812dd039b
Jeremy Caldwell <jeremy@eternalistic.net>
2013-06-19 07:43:49 -0700

    Added more base files

--- template.php
+++ template.php
@@ -226,6 +226,25 @@ function zen_subtheme_preprocess_block(&$variables, $hook) {
 // */
 
 
+/**
+ * Override or insert variables into the html template.
+ *
+ * @param $variables
+ *   An array of variables to pass to the theme template.
+ * @param $hook
+ *   The name of the template being rendered. This is usually "html", but can
+ *   also be "maintenance_page" since zen_preprocess_maintenance_page() calls
+ *   this function to have consistent variables.
+ */
+function zen_subtheme_preprocess_html(&$vars) {
+
+  // Add variables and paths needed for HTML5 and responsive support.
+  $vars['path_to_subtheme'] = drupal_get_path('theme', 'zen_subtheme');
+
+  // Apple touch icon
+  //$appleIcon = array('#tag' => 'link', '#attributes' => array('href' => $vars['path_to_subtheme'] . '/apple-touch-icon.png', 'rel' => 'apple-touch-icon'),);
+  //drupal_add_html_head($appleIcon, 'apple-touch-icon');
+}
 
 
 // Ouptuts site breadcrumbs with current page title appended onto trail
